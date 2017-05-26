0d2e38847607c71af47336b3d05db5a44f81b5f4
Will Kearney <wkearn@gmail.com>
2017-05-26 10:52:04 -0400

    Add some slightly more substantive tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,6 @@ using PIEMetData, TidalFluxExampleData
 using Base.Test
 
 setmetdatadir!(Pkg.dir("TidalFluxExampleData","data","met"))
-parsemet(2016)
+M = parsemet(2016)
+@test year.(M[:DateTime])[1] == 2016
+@test year.(M[:DateTime])[end] == 2017
